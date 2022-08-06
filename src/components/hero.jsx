6865b0d8e2c9ec4c74112886d68cf4f207eee3d5
davidelucifora <davidelucifora1@gmail.com>
2022-08-06 15:51:51 +0200

    Finished Hero component and created card component

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,1 +1,13 @@
 import React from 'react'
+import HeroImage from './../assets/hero-image.png'
+
+function Hero(){
+ 
+ return (   <div className="hero">
+        <img src={HeroImage} alt="Onlien Experiences" />
+        <h1>Online Experiences</h1>
+        <h5>Join unique interactive activities led by one-of-a-kind hosts all without leaving home.</h5>
+    </div>)
+}
+
+export default Hero
